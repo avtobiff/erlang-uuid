3f25ff870166306e4c7d6ae59ca31d03e544f61d
Per Andersson <avtobiff@gmail.com>
2012-02-23 12:59:33 +0100

    Add file header to include file

--- src/uuid.hrl
+++ src/uuid.hrl
@@ -1,3 +1,28 @@
+%% -----------------------------------------------------------------------------
+%% Copyright © 2010-2012 Per Andersson
+%%
+%% Erlang UUID is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% Erlang UUID is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with Erlang UUID.  If not, see <http://www.gnu.org/licenses/>.
+%% -----------------------------------------------------------------------------
+%% @author Per Andersson <avtobiff@gmail.com>
+%% @copyright 2010-2012 Per Andersson
+%% @doc
+%% Erlang UUID
+%%
+%% Include file for types, definitions, and macros.
+%% @end
+%% -----------------------------------------------------------------------------
+
 %% The UUID types
 -type uuid()::binary().
 -type uuid_string()::string().
