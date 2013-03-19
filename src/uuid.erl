77af39126224dac0cd4ec10469cc2a2953bdaadd
Ryan Flynn <parseerror+git@gmail.com>
2013-03-18 22:54:09 -0400

    simplify PidSum via builtins

--- src/uuid.erl
+++ src/uuid.erl
@@ -405,8 +405,6 @@ is_valid(_, _) -> false.
 %%      Used for random seed.
 -spec now_xor_pid() -> {pos_integer(), pos_integer(), pos_integer()}.
 now_xor_pid() ->
-    [_|PidPartsStr] =
-        re:split(pid_to_list(self()), "[<.>]", [{return, list}, trim]),
-    PidSum = lists:sum(lists:map(fun erlang:list_to_integer/1, PidPartsStr)),
+    PidSum = erlang:phash2(self()),
     {N0, N1, N2} = now(),
     {N0 bxor PidSum, N1 bxor PidSum, N2 bxor PidSum}.
