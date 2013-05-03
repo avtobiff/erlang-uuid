bc1d089fa96028f88630986c3589cf9fdc88f445
Per Andersson <avtobiff@gmail.com>
2013-05-03 09:34:56 +0200

    Export uuid types in module
    Move uuid types from include file to module.

--- src/uuid.hrl
+++ src/uuid.hrl
@@ -24,10 +24,6 @@
 %% @end
 %% -----------------------------------------------------------------------------
 
-%% The UUID types
--type uuid()::binary().
--type uuid_string()::string().
--type urn()::string().
 
 %% Variant, corresponds to variant 1 0 of RFC 4122.
 -define(VARIANT10, 2#10).
